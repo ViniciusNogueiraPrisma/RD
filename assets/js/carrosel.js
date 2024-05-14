0b4d79317e45264cfa0d335ff0b7d3d0186602d6
Vinicius Nogueira <v.francisco@riprisma.com>
2024-05-14 14:17:24 -0300

    teste iframe

--- assets/js/carrosel.js
+++ assets/js/carrosel.js
@@ -14,84 +14,104 @@ $(".toggle-mobile-menu").click(function () {
   $("body").toggleClass("open-menu");
 });
 
-$(".list-nav-glossario").slick({
-  slidesToShow: 26,
-  slidesToScroll: 1,
-  asNavFor: ".for-glossario-conteudo",
-  dots: false,
-  focusOnSelect: true,
-  infinite: false,
-  centerMode: false,
-  arrows: true,
-  waitForAnimate: false,
-  responsive: [
-    {
-      breakpoint: 1200,
-      settings: {
-        slidesToShow: 16,
-      },
-    },
-    {
-      breakpoint: 992,
-      settings: {
-        slidesToShow: 10,
-        centerMode: false,
-      },
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 6,
-        centerMode: false,
-      },
-    },
-    {
-      breakpoint: 475,
-      settings: {
-        slidesToShow: 4,
-        centerMode: false,
-      },
-    },
-  ],
-});
+//adição das classes em diferentes paginas aqui
+function addClassBanner() {
+  if (window.location.pathname === "/quem-somos") {
+    document
+      .querySelector(".itens-banner-interno")
+      .classList.add("itens-banner-qs");
+  } 
+  
+  else if (window.location.pathname === "/governanca-corporativa") {
+    document
+      .querySelector(".itens-banner-interno")
+      .classList.add("itens-full-gov");
+  }
+  
+}
 
-$(".for-glossario-conteudo").slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  adaptiveHeight: true,
-  infinite: false,
-  fade: true,
-  asNavFor: ".list-nav-glossario",
-  waitForAnimate: false,
-});
+addClassBanner();
 
-$(".carousel-timeline-content").slick({
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  infinite: false,
-  focusOnSelect: true,
+$(document).ready(function () {
+  $(".list-nav-glossario").slick({
+    slidesToShow: 26,
+    slidesToScroll: 1,
+    asNavFor: ".for-glossario-conteudo",
+    dots: false,
+    focusOnSelect: true,
+    infinite: false,
+    centerMode: false,
+    arrows: true,
+    waitForAnimate: false,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 16,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 10,
+          centerMode: false,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 6,
+          centerMode: false,
+        },
+      },
+      {
+        breakpoint: 475,
+        settings: {
+          slidesToShow: 4,
+          centerMode: false,
+        },
+      },
+    ],
+  });
 
-  arrows: false,
-  asNavFor: ".carousel-timeline-nav",
-  responsive: [
-    {
-      breakpoint: 991,
-      settings: {
-        slidesToShow: 3,
+  $(".for-glossario-conteudo").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    adaptiveHeight: true,
+    infinite: false,
+    fade: true,
+    asNavFor: ".list-nav-glossario",
+    waitForAnimate: false,
+  });
+
+  $(".carousel-timeline-content").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: false,
+    focusOnSelect: true,
+
+    arrows: false,
+    asNavFor: ".carousel-timeline-nav",
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+        },
       },
-    },
-    {
-      breakpoint: 767,
-      settings: {
-        slidesToShow: 2,
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+        },
       },
-    },
-    {
-      breakpoint: 475,
-      settings: {
-        slidesToShow: 1,
+      {
+        breakpoint: 475,
+        settings: {
+          slidesToShow: 1,
+        },
       },
-    },
-  ],
+    ],
+  });
 });
